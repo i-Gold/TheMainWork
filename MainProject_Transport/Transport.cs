9c69f4d2eb6def1ec299ec07d57495cc4a26d8e4
1 <1@1->
2017-10-13 23:13:38 +0300

    Add project files.

---
+++ MainProject_Transport/Transport.cs
@@ -0,0 +1,562 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public abstract class Transport
+    {
+        protected int speed;
+        protected string manufactor;
+        protected double weight;
+        protected double height;
+        protected Engine engine;
+        protected int amount;
+
+        public Transport()
+        {
+            manufactor = "unknown";
+        }
+
+        public Transport(int speed, string manufactor, double weight, double height, Engine engine, int amount)
+        {
+            this.speed = speed;
+            this.manufactor = manufactor;
+            this.weight = weight;
+            this.height = height;
+            this.engine = engine;
+            this.amount = amount;
+        }
+        
+        public int Speed
+        {
+            get
+            {
+                return speed;
+            }
+            set
+            {
+                speed = value;
+            }
+        }
+        public string Manufactor
+        {
+            get
+            {
+                return manufactor;
+            }
+            set
+            {
+                manufactor = value;
+            }
+        }
+        public double Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+        public Engine Engine
+        {
+            get
+            {
+                return engine;
+            }
+            set
+            {
+                engine = value;
+            }
+        }
+        public int Amount
+        {
+            get
+            {
+                return amount;
+            }
+            set
+            {
+                amount = value;
+            }
+        }
+
+        public virtual string getInformation()
+        {
+            return "Manufactor: " + manufactor
+                + " , speed: " + speed
+                + " , weight: " + weight
+                + " , height: " + height
+                + " , engine: " + engine.getInformation()
+                + " , amount: " + amount;
+        }
+
+        public virtual string infoToWrite()
+        {
+            return manufactor + "\t"
+                + speed + "\t"
+                + weight + "\t"
+                + height + "\t"
+                + engine.infoToWrite() + "\t"
+                + amount + "\t";
+        }
+    }
+
+    public abstract  class WaterTransport : Transport
+    {
+        public WaterTransport()
+        {
+            engine = new Disel();
+        }
+        public WaterTransport(int speed, string manufactor, double weight, double height, Engine engine, int amount)
+            : base(speed, manufactor, weight, height, engine, amount)
+        {
+
+        }
+    }
+
+    public abstract class AirTransport : Transport
+    {
+        public AirTransport()
+        {
+            engine = new ReactiveEngine();
+        }
+        public AirTransport(int speed, string manufactor, double weight, double height, Engine engine, int amount)
+            : base(speed, manufactor, weight, height, engine, amount)
+        {
+
+        }
+    }
+
+    public abstract class LandTransport : Transport
+    {
+        public LandTransport()
+        {
+            engine = new PetrolEngine();
+        }
+        public LandTransport(int speed, string manufactor, double weight, double height, Engine engine, int amount)
+            : base(speed, manufactor, weight, height, engine, amount)
+        {
+
+        }
+    }
+
+    public class Car : LandTransport
+    {
+        private string transmission;
+        private string body;
+
+        public Car() : base()
+        {
+            transmission = "unknown";
+            body = "unknown";
+        }
+        public Car(int speed, string manufactor, double weight, double height, 
+                Engine engine, int amount, string transmission, string body)
+                : base(speed, manufactor, weight, height, engine, amount)
+        {
+            this.transmission = transmission;
+            this.body = body;
+        }
+
+        public string Transmission
+        {
+            get
+            {
+                return transmission;
+            }
+            set
+            {
+                transmission = value;
+            }
+        }
+        public string Body
+        {
+            get
+            {
+                return body;
+            }
+            set
+            {
+                body = value;
+            }
+        }
+
+        public override string getInformation()
+        {
+            return base.getInformation()
+                + "Transmission: " + transmission
+                + " , body" + body;
+        }
+        public override string infoToWrite()
+        {
+            return this.GetType().BaseType.Name + "\t"
+                + this.GetType().Name + "\t"
+                + base.infoToWrite()
+                + "\t" + transmission
+                + "\t" + body;
+        }
+    }
+
+    public class Airplane : AirTransport
+    {
+        private string assignment;
+        private int weightBoard;
+
+        public Airplane()
+        {
+            assignment = "civil";
+        }
+        public Airplane(int speed, string manufactor, double weight, double height,
+                Engine engine, int amount, string assignment, int weightBoard)
+                : base(speed, manufactor, weight, height, engine, amount)
+        {
+            this.assignment = assignment;
+            this.weightBoard = weightBoard;
+        }
+
+        public string Assignment
+        {
+            get
+            {
+                return assignment;
+            }
+            set
+            {
+                assignment = value;
+            }
+        }
+        public int WeightBoard
+        {
+            get
+            {
+                return weightBoard;
+            }
+            set
+            {
+                weightBoard = value;
+            }
+        }
+
+        public override string getInformation()
+        {
+            return base.getInformation()
+                + "Assignment: " + assignment;
+        }
+        public override string infoToWrite()
+        {
+            return this.GetType().BaseType.Name + "\t"
+                + this.GetType().Name + "\t"
+                + base.infoToWrite()
+                + "\t" + assignment
+                + "\t" + weightBoard;
+        }
+    }
+
+    public class Ship : WaterTransport
+    {
+        private string cabin;
+
+        public Ship()
+        {
+            cabin = "standart";
+        }
+        public Ship(int speed, string manufactor, double weight, double height,
+                Engine engine, int amount, string cabin)
+                : base(speed, manufactor, weight, height, engine, amount)
+        {
+            this.cabin = cabin;
+        }
+
+        public string Cabin
+        {
+            get
+            {
+                return cabin;
+            }
+            set
+            {
+                cabin = value;
+            }
+        }
+
+        public override string getInformation()
+        {
+            return base.getInformation()
+                + "Cabin: " + cabin;
+        }
+        public override string infoToWrite()
+        {
+            return this.GetType().BaseType.Name + "\t"
+                + this.GetType().Name + "\t"
+                + base.infoToWrite()
+                + "\t" + cabin;
+        }
+    }
+
+    public class Train : LandTransport
+    {
+        private string category;
+
+        public Train()
+        {
+            category = "passenger";
+        }
+        public Train(int speed, string manufactor, double weight, double height,
+                Engine engine, int amount, string category)
+                : base(speed, manufactor, weight, height, engine, amount)
+        {
+            this.category = category;
+        }
+
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+            }
+        }
+
+        public override string getInformation()
+        {
+            return base.getInformation()
+                + "Category: " + category;
+        }
+        public override string infoToWrite()
+        {
+            return this.GetType().BaseType.Name + "\t"
+                + this.GetType().Name + "\t"
+                + base.infoToWrite()
+                + "\t" + category;
+        }
+    }
+
+    public class Bike : LandTransport
+    {
+        private string kind;
+
+        public Bike()
+        {
+            kind = "classic";
+        }
+        public Bike(int speed, string manufactor, double weight, double height,
+                Engine engine, int amount, string kind)
+                : base(speed, manufactor, weight, height, engine, amount)
+        {
+            this.kind = kind;
+        }
+
+        public string Kind
+        {
+            get
+            {
+                return kind;
+            }
+            set
+            {
+                kind = value;
+            }
+        }
+
+        public override string getInformation()
+        {
+            return base.getInformation()
+                + "Kind: " + kind;
+        }
+        public override string infoToWrite()
+        {
+            return this.GetType().BaseType.Name + "\t"
+                + this.GetType().Name + "\t"
+                + base.infoToWrite()
+                + "\t" + kind;
+        }
+    }
+
+    public abstract class Engine
+    {
+        protected double power;
+        protected string manufactor;
+
+        public Engine()
+        {
+            manufactor = "unknown";
+        }
+        public Engine(double power, string manufactor)
+        {
+            this.power = power;
+            this.manufactor = manufactor;
+        }
+
+        public double Power
+        {
+            get
+            {
+                return power;
+            }
+            set
+            {
+                power = value;
+            }
+        }
+        public string Manufactor
+        {
+            get
+            {
+                return manufactor;
+            }
+            set
+            {
+                manufactor = value;
+            }
+        }
+
+        public virtual string getInformation()
+        {
+            return "Power: " + power + "\t"
+                + "Manufactor: "  + manufactor;
+        }
+
+        public virtual string infoToWrite()
+        {
+            return power + "\t"
+                + manufactor;
+        }
+    }
+
+    public class PetrolEngine : Engine
+    {
+        private int capacity;
+
+        public PetrolEngine()
+        {
+
+        }
+        public PetrolEngine(double power, string manufactor, int capacity)
+                : base(power, manufactor)
+        {
+            this.capacity = capacity;
+        }
+
+        public int Capacity
+        {
+            get
+            {
+                return capacity;
+            }
+            set
+            {
+                capacity = value;
+            }
+        }
+
+        public override string getInformation()
+        {
+            return base.getInformation()
+                + "\t" + capacity;
+        }
+        public override string infoToWrite()
+        {
+            return this.GetType().Name + "\t"
+                + base.infoToWrite()
+                + "\t" + capacity;
+        }
+    }
+
+    public class Disel : Engine
+    {
+        private string type;
+
+        public Disel()
+        {
+            type = "unknown";
+        }
+        public Disel(double power, string manufactor, string type)
+                : base(power, manufactor)
+        {
+            this.type = type;
+        }
+
+        public string Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+
+        public override string getInformation()
+        {
+            return base.getInformation()
+                + "\t" + type;
+        }
+        public override string infoToWrite()
+        {
+            return this.GetType().Name + "\t"
+                + base.infoToWrite()
+                + "\t" + type + "\t";
+        }
+    }
+
+    public class ReactiveEngine : Engine
+    {
+        private string grade;
+
+        public ReactiveEngine()
+        {
+            grade = "air-breathing";
+        }
+        public ReactiveEngine(double power, string manufactor, string grade)
+                : base(power, manufactor)
+        {
+            this.grade = grade;
+        }
+
+        public string Grade
+        {
+            get
+            {
+                return grade;
+            }
+            set
+            {
+                grade = value;
+            }
+        }
+
+        public override string getInformation()
+        {
+            return base.getInformation()
+                + "\t" + grade;
+        }
+        public override string infoToWrite()
+        {
+            return this.GetType().Name + "\t"
+                + base.infoToWrite()
+                + "\t" + grade;
+        }
+    }
+}
---
+++ MainProject_Transport/Util.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+using System.IO;
+
+namespace Util
+{
+    class FileWork
+    {
+        private const string fileName = "file.txt";
+
+        public void writeAllToFile(List<Transport> list)
+        {
+            StreamWriter sw = new StreamWriter(fileName, true);
+            foreach(Transport t in list)
+            {
+                sw.WriteLine(t.infoToWrite());
+                Console.WriteLine(t.infoToWrite());
+            }
+            sw.Close();
+        }
+
+        public List<Transport> readAllFromFile()
+        {
+            List<Transport> result = new List<Transport>();
+
+            string[] fromFile = File.ReadAllLines(fileName);
+
+            foreach (string s in fromFile)
+            {
+                string[] items = s.Split('\t');
+
+                switch (items[1])
+                {
+                    case "Car":
+                    {
+                        getCarObject(items);
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        private Car getCarObject(string[] items)
+        {
+            Car car = new Car();
+
+            car.Manufactor = items[2];
+            car.Speed = int.Parse(items[3]);
+            car.Weight = double.Parse(items[4]);
+            car.Height = double.Parse(items[5]);
+            car.Engine = getEngine(items);
+            car.Amount = int.Parse(items[10]);
+            car.Transmission = items[11];
+            car.Body = items[12];
+
+            return car;
+        }
+
+        private Engine getEngine(string[] items)
+        {
+            Engine engine = null;
+
+            switch (items[6])
+            {
+                case "PetrolEngine":
+                {
+                    engine = getPetrolEngine(items);
+                    break;
+                }
+            }
+
+            return engine;
+        }
+
+        private PetrolEngine getPetrolEngine(string[] items)
+        {
+            PetrolEngine petrolEngine = new PetrolEngine();
+            petrolEngine.Power = double.Parse(items[7]);
+            petrolEngine.Manufactor = items[8];
+            petrolEngine.Capacity = int.Parse(items[9]);
+            
+            return petrolEngine;
+        }
+    }
+
+    public static class PrintWork
+    {
+        public static void printAll(List<Transport> list)
+        {
+            foreach(Transport t in list)
+            {
+                Console.WriteLine(t.getInformation());
+            }
+        }
+
+        public static void printByCategory(List<Transport> list, string category)
+        {
+            foreach (Transport t in list)
+            {
+                if (category.Trim().Equals(t.GetType().Name))
+                {
+                    Console.WriteLine(t.getInformation());
+                }
+            }
+        }
+    }
+
+    public static class SortWork
+    {
+        public static void sortByModel(List<Transport> list)
+        {
+            list.Sort((l1, l2) => l1.Manufactor.CompareTo(l2.Manufactor));
+        }
+
+        public static void sortBySpeed(List<Transport> list)
+        {
+            list.Sort((l1, l2) => l1.Speed.CompareTo(l2.Speed));
+        }
+
+        public static void sortByCategory(List<Transport> list)
+        {
+            list.Sort((l1, l2) => l1.GetType().Name.CompareTo(l2.GetType().Name));
+        }
+    }
+
+    public class SalesWork
+    {
+        public void sellItem(Transport transport, int amount)
+        {
+            int currentAmount = transport.Amount;
+            if(amount < 0 || amount > currentAmount)
+            {
+                throw new ArithmeticException();
+            }
+
+            transport.Amount -= amount;
+        }
+
+        public void buyItem(Transport transport, int amount)
+        {
+            transport.Amount += amount;
+        }
+    }
+
+    public class Menu
+    {
+        public void use()
+        {
+            SalesWork sw = new SalesWork();
+            FileWork fw = new FileWork();
+            List<Transport> worklist = new List<Transport>();
+            bool flag = true;
+            bool readFromFileFlag = false;
+            string choise;
+            Console.WriteLine("Welcome to our system!!!");
+
+            do
+            {
+                printMenu();
+                choise = Console.ReadLine();
+
+                switch (choise)
+                {
+                    case "1":
+                        {
+                            readFromFileItem(ref readFromFileFlag, worklist, fw);                           
+                            break;
+                        }
+                    case "2":
+                        {
+                            printAllInformationItem(worklist);
+                            break;
+                        }
+                    case "3.1":
+                        {
+                            SortWork.sortByModel(worklist);
+                            PrintWork.printAll(worklist);
+                            break;
+                        }
+                    case "6":
+                        {
+                            worklist.Add(addNewItem());
+                            break;
+                        }
+                }
+
+            } while (flag);  
+        }
+
+        private void printMenu()
+        {
+            Console.WriteLine("Make choise:");
+            Console.WriteLine("1. Read all from file");
+            Console.WriteLine("2. Print all information");
+            Console.WriteLine("3. Sorting");
+            Console.WriteLine("3.1 \t Sorting by model");
+            Console.WriteLine("3.2 \t Sorting by speed");
+            Console.WriteLine("3.3 \t Sorting by category");
+            Console.WriteLine("4. Sell item");
+            Console.WriteLine("5. Buy item");
+            Console.WriteLine("6. Add item");
+            Console.WriteLine("0. Exit");
+        }
+
+        private void readFromFileItem(ref bool readFromFileFlag, List<Transport> worklist, FileWork fw)
+        {
+            if (!readFromFileFlag)
+            {
+                worklist.AddRange(fw.readAllFromFile());
+                readFromFileFlag = true;
+                Console.WriteLine("Database loaded");
+            }
+            else
+            {
+                Console.WriteLine("Database is up to date");
+            }
+        }
+
+        private void printAllInformationItem(List<Transport> worklist)
+        {
+            if (worklist.Count == 0)
+            {
+                Console.WriteLine("List is empty. Maybe you haven't load database. You can do it with Menu");
+            }
+            else
+            {
+                PrintWork.printAll(worklist);
+            }
+        }
+
+        private Transport addNewItem()
+        {
+            Console.WriteLine("Choose your transport:");
+            Console.WriteLine("1. Car");
+
+            Transport transport = null;
+            string choise = Console.ReadLine();
+            switch (choise)
+            {
+                case "1":
+                    {
+                        transport = addCar();
+                        break;
+                    }
+            }
+
+            return transport;
+        }
+
+        private Car addCar()
+        {
+            Car car = new Car();
+            Console.WriteLine("Enter speed: ");
+            car.Speed = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter transmission: ");
+            car.Transmission = Console.ReadLine();
+            car.Engine = new Disel();
+
+            return car;
+        }
+    }
+}
